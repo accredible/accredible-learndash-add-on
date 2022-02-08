e9f8dd7601c943573cf333ed737da682ef0ee90c
kei178 <keisuke.inaba@hey.com>
2022-02-08 16:04:05 +0900

    Add docker development setup

---
+++ accredible-learndash-add-on.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Accredible LearnDash Add-on
+ *
+ * @package Accredible_Learndash_Add_On
+ *
+ * Plugin Name: Accredible LearnDash Add-on
+ * Plugin URI:  https://github.com/accredible/accredible-learndash-add-on
+ * Description: This is a plugin description.
+ * Version:     1.0.0
+ * Author:      Accredible
+ * Author URI:  https://www.accredible.com/
+ */
