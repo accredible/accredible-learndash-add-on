fade5e1832bbec7809d15340ff1dffbdcb7c28e8
kei178 <keisuke.inaba@hey.com>
2022-02-17 15:41:13 +0900

    Add plugin custom DB tables

--- tests/includes/test-class-accredible-learndash-admin-database.php
+++ tests/includes/test-class-accredible-learndash-admin-database.php
@@ -1,42 +1,46 @@
 <?php
 /**
- * Class Accredible_Learndash_Unintall_Test
+ * Class Accredible_Learndash_Admin_Database_Test
  *
  * @package Accredible_Learndash_Add_On
  */
 
 defined( 'ABSPATH' ) || die;
 
-/**
- * Unit tests for the plugin main file
- */
-class Accredible_Learndash_Uninstall_Test extends WP_UnitTestCase {
-	/**
-	 * Test uninstallation
-	 */
-	public function test_uninstall() {
-		// Set plugin options.
-		$api_key_option       = 'accredible_learndash_api_key';
-		$server_region_option = 'accredible_learndash_server_region';
-		update_option( $api_key_option, 'my_api_key' );
-		update_option( $server_region_option, 'us' );
+require_once ACCREDILBE_LEARNDASH_PLUGIN_PATH . '/includes/class-accredible-learndash-admin-database.php';
 
-		// Create plugin custom DB tables.
+/**
+ * Unit tests for Accredible_Learndash_Admin_Database
+ */
+class Accredible_Learndash_Admin_Database_Test extends WP_UnitTestCase {
+	/**
+	 * Test if it creates plugin custom DB tables.
+	 */
+	public function test_setup() {
+		// Make sure if tables have not been created.
+		foreach ( Accredible_Learndash_Admin_Database::TABLE_NAMES as $table_name ) {
+			$this->assertFalse( $this->table_exists( $table_name ) );
+		}
+
+		// XXX Stop transforming all `CREATE TABLE` to `CREATE TEMPORARY TABLE`.
+		remove_filter( 'query', array( $this, '_create_temporary_tables' ) );
+		Accredible_Learndash_Admin_Database::setup();
+		foreach ( Accredible_Learndash_Admin_Database::TABLE_NAMES as $table_name ) {
+			$this->assertTrue( $this->table_exists( $table_name ) );
+		}
+	}
+
+	/**
+	 * Test if it drops all plugin custom DB tables.
+	 */
+	public function test_drop_all() {
 		// XXX Stop transforming all `CREATE TABLE` to `CREATE TEMPORARY TABLE`.
 		remove_filter( 'query', array( $this, '_create_temporary_tables' ) );
 		Accredible_Learndash_Admin_Database::setup();
 
 		// XXX Stop transforming all `DROP TABLE` to `DROP TEMPORARY TABLE`.
 		remove_filter( 'query', array( $this, '_drop_temporary_tables' ) );
-		// Run the uninstall script.
-		define( 'WP_UNINSTALL_PLUGIN', true ); // phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
-		include plugin_dir_path( __DIR__ ) . '/uninstall.php';
-
-		// Plugin options to be empty.
-		$this->assertFalse( get_option( $api_key_option ) );
-		$this->assertFalse( get_option( $server_region_option ) );
-
-		// Plugin custom DB tables to be dropped.
+		Accredible_Learndash_Admin_Database::drop_all();
 		foreach ( Accredible_Learndash_Admin_Database::TABLE_NAMES as $table_name ) {
 			$this->assertFalse( $this->table_exists( $table_name ) );
 		}
--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,8 @@
 
 defined( 'WP_UNINSTALL_PLUGIN' ) || die;
 
-// TODO: NTGR-522 Destroy custom database tables.
+require_once plugin_dir_path( __FILE__ ) . '/includes/class-accredible-learndash-admin-database.php';
+Accredible_Learndash_Admin_Database::drop_all();
 
 require_once plugin_dir_path( __FILE__ ) . '/includes/class-accredible-learndash-admin-setting.php';
 Accredible_Learndash_Admin_Setting::delete_options();
