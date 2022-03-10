5ee67998e6bad20db75f48076a0160844fc1499f
Martin Muhinga <muhingan@gmail.com>
2022-03-10 16:52:07 +0300

    Issuance list - updated template, added table styles

--- includes/templates/admin-issuance-list.php
+++ includes/templates/admin-issuance-list.php
@@ -18,7 +18,7 @@ require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-iss
 	<div class="accredible-content">
 		<table class="accredible-table">
 			<thead>
-				<tr>
+				<tr class="accredible-header-row">
 					<th></th>
 					<th>Course Name</th>
 					<th>Accredilbe Group</th>
@@ -28,36 +28,44 @@ require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-iss
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
+				<tr class="accredible-row">
 					<td>1</td>
 					<td>Course 110</td>
 					<td>Course 100 Series</td>
 					<td>Course Completion</td>
-					<td>17 Feb 2022 13:09 PM </td>
+					<td>
+						<span>17 Feb 2022</span> <span class="accredible-cell-time">13:09 PM</span>
+					</td>
 					<td></td>
 				</tr>
-				<tr>
+				<tr class="accredible-row">
 					<td>2</td>
 					<td>Course 105</td>
 					<td>Course 100 Series</td>
 					<td>Course Completion</td>
-					<td>10 Jan 2022 11:45 AM </td>
+					<td>
+						<span>10 Jan 2022</span> <span class="accredible-cell-time">11:45 AM</span>
+					</td>
 					<td></td>
 				</tr>
-				<tr>
+				<tr class="accredible-row">
 					<td>3</td>
 					<td>Course 104</td>
 					<td>Course 100 Series</td>
 					<td>Course Completion</td>
-					<td>08 Jan 2022 10:07 AM </td>
+					<td>
+						<span>08 Jan 2022</span> <span class="accredible-cell-time">10:07 AM</span>
+					</td>
 					<td></td>
 				</tr>
-				<tr>
+				<tr class="accredible-row">
 					<td>4</td>
 					<td>Course 213</td>
 					<td>Course 200 Series</td>
 					<td>Course Completion</td>
-					<td>01 Dec 2021 09:31 AM </td>
+					<td>
+						<span>01 Dec 2021</span> <span class="accredible-cell-time">09:31 AM</span>
+					</td>
 					<td></td>
 				</tr>
 			</tbody>
