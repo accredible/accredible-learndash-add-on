8d94926f4a53d026252102fe8321d71a85104c57
Martin Muhinga <muhingan@gmail.com>
2022-03-10 16:11:42 +0300

    Issuance list - add static data to template for styling

--- includes/templates/admin-issuance-list.php
+++ includes/templates/admin-issuance-list.php
@@ -17,7 +17,50 @@ require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-iss
 	</div>
 	<div class="accredible-content">
 		<table class="accredible-table">
-			<tr></tr>
+			<thead>
+				<tr>
+					<th></th>
+					<th>Course Name</th>
+					<th>Accredilbe Group</th>
+					<th>Issuance Trigger</th>
+					<th>Date Created</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Course 110</td>
+					<td>Course 100 Series</td>
+					<td>Course Completion</td>
+					<td>17 Feb 2022 13:09 PM </td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Course 105</td>
+					<td>Course 100 Series</td>
+					<td>Course Completion</td>
+					<td>10 Jan 2022 11:45 AM </td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Course 104</td>
+					<td>Course 100 Series</td>
+					<td>Course Completion</td>
+					<td>08 Jan 2022 10:07 AM </td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Course 213</td>
+					<td>Course 200 Series</td>
+					<td>Course Completion</td>
+					<td>01 Dec 2021 09:31 AM </td>
+					<td></td>
+				</tr>
+			</tbody>
 		</table>
-    </div>
+	</div>
 </div>
