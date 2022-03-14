a7eff7a8523646800f79943a57aef17ee5129e14
Martin Muhinga <muhingan@gmail.com>
2022-03-14 17:07:31 +0300

    Issuance form - added form, select courses dropdown

--- includes/templates/admin-auto-issuance-form.php
+++ includes/templates/admin-auto-issuance-form.php
@@ -8,11 +8,39 @@
 defined( 'ABSPATH' ) || die;
 
 require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-auto-issuance.php';
+
+$accredible_learndash_form_action = 'action=add_auto_issuance'; // TODO - or 'action=edit_auto_issuance&id=$issuance_id' if auto_issuance is available.
+$accredible_learndash_courses     = apply_filters( 'accredible_learndash_get_courses', '' );
 ?>
 
 <div class="accredible-wrapper">
 	<div class="accredible-header-tile">
 		<h1 class="title"><?php esc_html_e( 'Configure Auto Issuance' ); ?></h1>
+
+		<a	href="admin.php?page=accredible_learndash_issuance_list" 
+			class="button accredible-button-outline-natural accredible-button-large"><?php esc_html_e( 'Cancel' ); ?></a>
 	</div>
-	<div class="accredible-content"></div>
+	<div class="accredible-content">
+		<form
+			action="admin.php?page=accredible_learndash_admin_action&<?php echo $accredible_learndash_form_action; ?>"
+			method="post">
+				<div class="accredible-form-field">
+					<label for="accredible_learndash_course"><?php esc_html_e( 'Select a course' ); ?></label>
+					
+					<select id="accredible_learndash_course" name="accredible_learndash_course">
+						<?php foreach ( $accredible_learndash_courses as $accredible_learndash_key => $accredible_learndash_value ) : ?>
+							<option value="<?php echo esc_attr( $accredible_learndash_key ); ?>">
+								<?php echo esc_html( $accredible_learndash_value ); ?>
+							</option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+
+				<div class="accredible-form-field">
+					<label for="accredible_learndash_credential_group"><?php esc_html_e( 'Select the credential group' ); ?></label>
+				</div>
+
+				<?php submit_button( 'Save', 'accredible-button-primary accredible-button-large', 'submit', false ); ?>
+			</form>
+	</div>
 </div>
