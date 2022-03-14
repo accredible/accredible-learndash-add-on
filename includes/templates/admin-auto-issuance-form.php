df8982205175fd030634feeee73b961d97f6bab0
Martin Muhinga <muhingan@gmail.com>
2022-03-14 17:29:34 +0300

    Issuance form - added groups select dropdown

--- includes/templates/admin-auto-issuance-form.php
+++ includes/templates/admin-auto-issuance-form.php
@@ -11,6 +11,7 @@ require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-aut
 
 $accredible_learndash_form_action = 'action=add_auto_issuance'; // TODO - or 'action=edit_auto_issuance&id=$issuance_id' if auto_issuance is available.
 $accredible_learndash_courses     = apply_filters( 'accredible_learndash_get_courses', '' );
+$accredible_learndash_groups      = apply_filters( 'accredible_learndash_get_groups', array() );
 ?>
 
 <div class="accredible-wrapper">
@@ -22,7 +23,7 @@ $accredible_learndash_courses     = apply_filters( 'accredible_learndash_get_cou
 	</div>
 	<div class="accredible-content">
 		<form
-			action="admin.php?page=accredible_learndash_admin_action&<?php echo $accredible_learndash_form_action; ?>"
+			action="admin.php?page=accredible_learndash_admin_action&<?php echo esc_attr( $accredible_learndash_form_action ); ?>"
 			method="post">
 				<div class="accredible-form-field">
 					<label for="accredible_learndash_course"><?php esc_html_e( 'Select a course' ); ?></label>
@@ -37,7 +38,15 @@ $accredible_learndash_courses     = apply_filters( 'accredible_learndash_get_cou
 				</div>
 
 				<div class="accredible-form-field">
-					<label for="accredible_learndash_credential_group"><?php esc_html_e( 'Select the credential group' ); ?></label>
+					<label for="accredible_learndash_group"><?php esc_html_e( 'Select the credential group' ); ?></label>
+
+					<select id="accredible_learndash_group" name="accredible_learndash_group">
+						<?php foreach ( $accredible_learndash_groups as $accredible_learndash_key => $accredible_learndash_value ) : ?>
+							<option value="<?php echo esc_attr( $accredible_learndash_key ); ?>">
+								<?php echo esc_html( $accredible_learndash_value ); ?>
+							</option>
+						<?php endforeach; ?>
+					</select>
 				</div>
 
 				<?php submit_button( 'Save', 'accredible-button-primary accredible-button-large', 'submit', false ); ?>
