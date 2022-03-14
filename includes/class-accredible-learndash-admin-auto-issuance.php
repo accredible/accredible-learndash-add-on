ccb2fb14804a6b8e478562771d4b5f7a7bcb999f
Martin Muhinga <muhingan@gmail.com>
2022-03-14 12:10:31 +0300

    Issuance form - added template and class files

---
+++ includes/class-accredible-learndash-admin-auto-issuance.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Accredible LearnDash Add-on admin auto issuance class
+ *
+ * @package Accredible_Learndash_Add_On
+ */
+
+defined( 'ABSPATH' ) || die;
+
+if ( ! class_exists( 'Accredible_Learndash_Admin_Auto_Issuance' ) ) :
+	/**
+	 * Accredible LearnDash Add-on admin auto issuance class
+	 */
+	class Accredible_Learndash_Admin_Auto_Issuance {
+	}
+endif;
---
+++ includes/templates/admin-auto-issuance-form.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Accredible LearnDash Add-on admin auto issuance form page template.
+ *
+ * @package Accredible_Learndash_Add_On
+ */
+
+defined( 'ABSPATH' ) || die;
+
+require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-auto-issuance.php';
+?>
+
+<div class="accredible-wrapper">
+	<div class="accredible-header-tile">
+		<h1 class="title"><?php esc_html_e( 'Auto Issuance' ); ?></h1>
+	</div>
+	<div class="accredible-content"></div>
+</div>
