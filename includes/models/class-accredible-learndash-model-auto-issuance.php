c48a34e4183407611e57914e249aeccf27cfe3bb
Martin Muhinga <muhingan@gmail.com>
2022-03-22 20:13:52 +0300

    Model_Auto_Issuance - added  search_groups_ajax method
    - this method is called by the groups autocomplete

--- includes/models/class-accredible-learndash-model-auto-issuance.php
+++ includes/models/class-accredible-learndash-model-auto-issuance.php
@@ -22,5 +22,39 @@ if ( ! class_exists( 'Accredible_Learndash_Model_Auto_Issuance' ) ) :
 			global $wpdb;
 			return $wpdb->prefix . Accredible_Learndash_Admin_Database::AUTO_ISSUANCES_TABLE_NAME;
 		}
+
+		/**
+		 * Get group options. This method is only called via ajax.
+		 *
+		 * @return array
+		 */
+		public static function search_groups_ajax() {
+			$groups   = array();
+			$response = array();
+
+			if ( isset( $_REQUEST['search_term'] ) && ! empty( $_REQUEST['search_term'] ) ) {
+				$search_term = $_REQUEST['search_term'];
+				$api_client  = new Accredible_Learndash_Api_V1_Client();
+				$response    = $api_client->get_groups( $search_term );
+			}
+
+			if ( ! isset( $response['errors'] ) ) {
+				foreach ( $response['groups'] as $value ) {
+					array_push(
+						$groups,
+						array( 
+							'value' => $value['id'],
+							'label' => $value['name'],
+						)
+					);
+				}
+
+				wp_send_json_success( $groups );
+			} else {
+				wp_send_json_error();
+			}
+
+			wp_die();
+		}
 	}
 endif;
