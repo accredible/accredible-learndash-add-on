3977c710c9c5083082e3e518ba40a97475a1046e
Martin Muhinga <muhingan@gmail.com>
2022-03-23 12:51:18 +0300

    Issuance Form - fill group in autocomplete during editing auto issuance

--- includes/templates/admin-auto-issuance-form.php
+++ includes/templates/admin-auto-issuance-form.php
@@ -8,12 +8,14 @@
 defined( 'ABSPATH' ) || die;
 
 require_once plugin_dir_path( __DIR__ ) . '/class-accredible-learndash-admin-auto-issuance.php';
+require_once plugin_dir_path( __DIR__ ) . '/helpers/class-accredible-learndash-admin-form-helper.php';
 
 $accredible_learndash_courses = Accredible_Learndash_Admin_Auto_Issuance::get_course_options();
 
 $accredible_learndash_issuance_current_page = isset( $_GET['page_num'] ) ? esc_attr( wp_unslash( $_GET['page_num'] ) ) : 1; // phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Recommended
 $accredible_learndash_issuance_id           = isset( $_GET['id'] ) ? esc_attr( wp_unslash( $_GET['id'] ) ) : null; // phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Recommended
 
+$accredible_learndash_group       = null;
 $accredible_learndash_form_action = 'add_auto_issuance';
 $accredible_learndash_issuance    = array(
 	'id'                  => null,
@@ -25,7 +27,21 @@ if ( ! is_null( $accredible_learndash_issuance_id ) ) {
 	/** TODO NTGR-519: get issuance by issuance id.
 	$accredible_learndash_issuance = Accredible_Learndash_Model_Auto_Issuance::get_results();
 	*/
+
+	// Fetch saved group by id to fill autocomplete.
+	if ( ! empty( $accredible_learndash_issuance['accredible_group_id'] ) ) {
+		$accredible_learndash_client    = new Accredible_Learndash_Api_V1_Client();
+		$accredible_learndash_group_res = $accredible_learndash_client->get_group( 323330 );
+		if ( ! isset( $accredible_learndash_group_res['errors'] ) ) {
+			$accredible_learndash_group = array(
+				'id'   => $accredible_learndash_group_res['group']['id'],
+				'name' => $accredible_learndash_group_res['group']['name'],
+			);
+		}
+	}
 }
+
+
 ?>
 
 <div class="accredible-wrapper">
@@ -73,9 +89,18 @@ if ( ! is_null( $accredible_learndash_issuance_id ) ) {
 				<div class="accredible-form-field">
 					<label for="accredible_learndash_group"><?php esc_html_e( 'Select the credential group' ); ?></label>
 
-					<input type='text' id='accredible_learndash_group_autocomplete' required/>
+					<input
+						type='text'
+						id='accredible_learndash_group_autocomplete' 
+						<?php Accredible_Learndash_Admin_Form_Helper::value_attr( $accredible_learndash_group['name'] ); ?> 
+						required/>
 
-					<input type="hidden" id="accredible_learndash_group" name="accredible_learndash_group"/>
+					<input 
+						type="hidden"
+						id="accredible_learndash_group"
+						name="accredible_learndash_group"
+						<?php Accredible_Learndash_Admin_Form_Helper::value_attr( $accredible_learndash_group['id'] ); ?>
+						readonly/>
 				</div>
 
 				<?php submit_button( 'Save', 'accredible-button-primary accredible-button-large', 'submit', false ); ?>
