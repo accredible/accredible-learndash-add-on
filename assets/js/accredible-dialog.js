e417a9d47b48adb4fc2181d8ea360ed3d883ce7a
Martin Muhinga <muhingan@gmail.com>
2022-04-05 14:43:45 +0300

    Dialog - updated script to open confirm dialog

--- assets/js/accredible-dialog.js
+++ assets/js/accredible-dialog.js
@@ -1,28 +1,50 @@
 jQuery( function(){
-    if (jQuery('#accredible_learndash_dialog').length ) {
-        jQuery('#accredible_learndash_dialog').dialog({
-            modal: true,
-            draggable: false,
-            minWidth: 400,
-            classes: {
-                'ui-dialog': 'accredible-dialog'
-            },
-            buttons: [
-                {
-                    text: "Cancel",
-                    class: 'accredible-button-flat-natural accredible-button-small',
-                    click: function() {
-                        jQuery(this).dialog("close");
-                    }
-                },
-                {
-                    text: "Delete",
-                    class: 'button accredible-button-primary accredible-button-small',
-                    click: function() {
-                        jQuery(this).dialog("close");
-                    }
+    const dialog = jQuery('<div></div>').dialog({
+        appendTo: '#wpcontent',
+        modal: true,
+        draggable: false,
+        autoOpen: false,
+        minWidth: 400,
+        classes: {
+            'ui-dialog': 'accredible-dialog'
+        },
+        buttons: [
+            {
+                text: "Cancel",
+                class: 'accredible-button-flat-natural accredible-button-large',
+                click: function() {
+                    jQuery(this).dialog("close");
                 }
-            ]
-        });
-    }
+            }
+        ]
+    });
+
+    jQuery('[data-accredible-dialog]').on('click', function(event){
+        const element = this;
+        if (jQuery(element).data('accredibleDialog')) {
+            event.preventDefault();
+
+            // Set dialog options.
+            const options = dialog.dialog('option');
+            dialog.html('<p>Are you sure you want to delete this auto issuance?</p>');
+            options.title = 'Delete auto issuance';
+            options.buttons[1] = {
+                text: "Delete",
+                class: 'button accredible-button-primary accredible-button-large',
+                click: function() {
+                    jQuery(this).dialog("close");
+                }
+            };
+            
+            if (element.tagName === 'A') {
+                const href = element.href;
+                options.buttons[1].click = function() {
+                    window.location.href = href;
+                }
+            }
+
+            dialog.dialog('option', options);
+            dialog.dialog('open');
+        }
+    });
 });
