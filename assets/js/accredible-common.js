f8be0141fa2c6137debc21eadd76f9ee1b9f3a5e
Martin Muhinga <muhingan@gmail.com>
2022-04-12 13:43:16 +0300

    added doAutoIssuanceAction ajax call

--- assets/js/accredible-common.js
+++ assets/js/accredible-common.js
@@ -7,4 +7,12 @@ jQuery(function(){
         };
         return jQuery.post(accredibledata.ajaxurl, post_data).then();
     };
+
+    accredibleAjax.doAutoIssuanceAction = function(formData) {
+        var post_data = {
+            'action': 'accredible_learndash_ajax_handle_auto_issuance_action',
+        };
+        post_data = Object.assign(post_data, formData);
+        return jQuery.post(accredibledata.ajaxurl, post_data).then();
+    };
 });
